db75fe9a0092b4c77674a8aa7d8d310758cf38f8
pabl1ku <115459058+pabl1ku@users.noreply.github.com>
2023-02-27 23:03:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from operator import gt
 from gtts import gTTS
 
 
-language = "es" 
-#Aquí puedes cambiar el idioma.
+language = "es"  #Aquí puedes cambiar el idioma.
 
-text = "Hola mundo, ¿Donde vives actualmente?"
-#Introduce el texto que quieras convertir.
+text = "Hola mundo, ¿Donde vives actualmente?" #Introduce el texto que quieras convertir.
 
 speech = gTTS(text=text, lang=language, slow=False)
+
+speech.save("output.mp3")
